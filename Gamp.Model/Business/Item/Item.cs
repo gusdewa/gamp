0c98836da0f92ceccb339431017d0d719278467b
gusdewa <gusdewa@gmail.com>
2014-12-10 15:28:59 +0700

    Add some models

---
+++ Gamp.Model/Business/Item/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Gamp.Model.Business.Item
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+    }
+}
