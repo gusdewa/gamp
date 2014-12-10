157b2cea9dd8b576c20e131340f058ed7a89ef66
gusdewa <gusdewa@gmail.com>
2014-12-10 16:56:32 +0700

    Add Item Models

--- Gamp.Model/Business/Item/Item.cs
+++ Gamp.Model/Business/Item/Item.cs
@@ -1,9 +1,14 @@
-﻿namespace Gamp.Model.Business.Item
+﻿using System;
+
+namespace Gamp.Model.Business.Item
 {
     public class Item
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double UnitPrice { get; set; }
+        public double SellingPrice { get; set; }
+        public double Discount { get; set; }
+        public DateTime PromoStartDate { get; set; }
+        public DateTime PromoEndDate { get; set; }
     }
 }
